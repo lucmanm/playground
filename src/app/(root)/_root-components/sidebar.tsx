ac0ad644e2845c509c9c30ee11ccfb3978e44d79
lucmanm <greenjamez22@gmail.com>
2024-05-11 13:36:12 +0300

    sidebar menu updated

--- src/app/(root)/_root-components/sidebar.tsx
+++ src/app/(root)/_root-components/sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Tooltip,
@@ -17,8 +18,36 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
+const menuData = [
+  {
+    name: "Projects",
+    url: "/project",
+    icon: <SquareTerminal className="size-5" />,
+  },
+  {
+    name: "Models",
+    url: "/model",
+    icon: <Bot className="size-5" />,
+  },
+  {
+    name: "Api",
+    url: "/api",
+    icon: <Code2 className="size-5" />,
+  },
+  {
+    name: "Documentation",
+    url: "/documentation",
+    icon: <Book className="size-5" />,
+  },
+  {
+    name: "Settings",
+    url: "/settings",
+    icon: <Settings2 className="size-5" />,
+  },
+];
 export default function Sidebar() {
+  const pathname = usePathname();
   return (
     <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
       <div className="border-b p-2">
@@ -40,84 +69,27 @@ export default function Sidebar() {
           </Link>
         </Button>
       </div>
+      {/* Menu */}
       <nav className="grid gap-1 p-2">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link href="/">
-              <Button
-                variant="ghost"
-                size="icon"
-                className="rounded-lg bg-muted"
-                aria-label="Playground"
-              >
-                <SquareTerminal className="size-5" />
-              </Button>
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Projects
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="Models"
-            >
-              <Bot className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Models
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="API"
-            >
-              <Code2 className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            API
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="Documentation"
-            >
-              <Book className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Documentation
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="Settings"
-            >
-              <Settings2 className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Settings
-          </TooltipContent>
-        </Tooltip>
+        {menuData.map((data, index) => (
+          <Tooltip key={index}>
+            <TooltipTrigger asChild >
+              <Link href={`${data.url}`}>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className={`rounded-lg hover:bg-muted data-[state=active]:bg-black`}
+                  aria-label={data.name}
+                >
+                  {data.icon}
+                </Button>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right" sideOffset={5}>
+              {data.name}
+            </TooltipContent>
+          </Tooltip>
+        ))}
       </nav>
       <nav className="mt-auto grid gap-1 p-2">
         <Tooltip>
