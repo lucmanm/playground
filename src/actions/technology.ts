3fee6486bb2643a9ce18f6b64e2750f76e6992ff
lucmanm <greenjamez22@gmail.com>
2024-05-23 00:01:07 +0300

    applied sync data or set defaut data

--- src/actions/technology.ts
+++ src/actions/technology.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
@@ -8,25 +9,54 @@ const techDefaultData = [
     "React", "Javascript", "HTML"
 ] as const
 
+const projectDefaultData = {
+    title: "www.compu-tech.com.sa",
+    description: "A website electronic from jeddah",
+    technology: ["React", "Javascript"]
+
+}
+
+
 const techDefaultDataSchema = z.string().array()
 
-export  async function createSyncTechnology() {
+export async function createSyncTechnology() {
     try {
+        const session = await auth()
+        const user = session?.user
 
-        const data =  techDefaultDataSchema.parse(techDefaultData)
-        // const checkData = await prisma.technology.findMany({
-        //     where: {
-        //         name: data.map(data =>(data)) 
-        //     }
-        // })
-        const status = await prisma.technology.createMany({
-            data: data.map(data => ({
-                name: data
-            })),
-            skipDuplicates: true
-        })
-        if (status) {
-            return {message :"Sucessfully Syncronized"}
+        const data = techDefaultDataSchema.parse(techDefaultData)
+
+        if (user?.role === "ADMIN") {
+
+            const status = await prisma.technology.createMany({
+                data: data.map(data => ({
+                    name: data
+                })),
+                skipDuplicates: true
+            })
+            if (user.id) {
+
+                await prisma.project.create({
+                    data: {
+                        name: projectDefaultData.title,
+                        description: projectDefaultData.description,
+                        userId: user.id,
+                        technology: {
+                            connect: projectDefaultData.technology.map(data => ({
+                                name: data
+                            }))
+                        }
+                    },
+
+                })
+
+
+            }
+            revalidatePath("/")
+            if (status) {
+                return { message: "Sucessfully Syncronized" }
+            }
+
         }
 
     } catch (error) {
--- src/app/(root)/[crudproduct]/add-edit-project-form.tsx
+++ src/app/(root)/[crudproduct]/add-edit-project-form.tsx
@@ -12,16 +12,18 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { useRouter } from "next/navigation";
 import { Checkbox } from "@/components/ui/checkbox";
 import { createProject, updateProject } from "@/actions/project";
-import {  defaultProjectSchema, defaultTechnologySchema } from "@/type/validation";
+import { defaultProjectSchema, defaultTechnologySchema } from "@/type/validation";
 
 export const defaultFormProjectSchema = z.object({
   name: z.string().min(1, "Please enter title of your project"),
   description: z.string().min(1, "Please enter description of your project"),
-  technology: z.array(z.object({
+  technology: z.array(
+    z.object({
       name: z.array(z.string()).refine((value) => value.some((item) => item), {
-          message: "You have to select at least one item.",
-        })
-  }))
+        message: "You have to select at least one item.",
+      }),
+    })
+  ),
 });
 
 type TAddEditProjectForm = {
@@ -38,30 +40,32 @@ const AddEditProjectForm: React.FC<TAddEditProjectForm> = ({ data, technology })
   const form = useForm<z.infer<typeof defaultFormProjectSchema>>({
     resolver: zodResolver(defaultFormProjectSchema),
     defaultValues: data
-      ? { ...data, technology }
+      ? { ...data, technology: data?.technology?.length ? data.technology : [] }
       : {
           name: "",
           description: "",
-          technology: [],
+          technology: [""],
         },
   });
 
   const onSubmit = async (values: z.infer<typeof defaultFormProjectSchema>) => {
     try {
-      if (data) {
-        await updateProject(values);
-        toast({
-          description: `Succesfully Updated.`,
-          variant: "success",
-        });
-      } else {
-        await createProject(values);
-        toast({
-          description: `Succesfully Created.`,
-          variant: "success",
-        });
-      }
-      router.push("/");
+      console.log(values.technology);
+
+      // if (data) {
+      //   await updateProject(values);
+      //   toast({
+      //     description: `Succesfully Updated.`,
+      //     variant: "success",
+      //   });
+      // } else {
+      //   await createProject(values);
+      //   toast({
+      //     description: `Succesfully Created.`,
+      //     variant: "success",
+      //   });
+      // }
+      // router.push("/");
     } catch (error) {
       console.log("ERROR_SUBMIT_FORM", error);
     }
@@ -112,37 +116,30 @@ const AddEditProjectForm: React.FC<TAddEditProjectForm> = ({ data, technology })
 
             {/* check for selection of tech stacks */}
 
-              <FormField
+            <FormField
               control={form.control}
               name="technology"
               render={() => (
                 <FormItem>
                   <div className="mb-4">
                     <FormLabel className="text-base">Sidebar</FormLabel>
-                    
+
                     <FormDescription>
-                    {
-                      data?.technology.map(data => data.name)
-                    }
+                      {data?.technology.map((data) => data.name)}
                       Select the items you want to display in the sidebar.
                     </FormDescription>
                   </div>
                   {technology.map((item) => (
                     <FormField
-                      key={item.id}
+                      key={item.name}
                       control={form.control}
                       name="technology"
                       render={({ field }) => {
                         return (
-                          <FormItem
-                            key={item.id}
-                            className="flex flex-row items-start space-x-3 space-y-0"
-                          >
+                          <FormItem key={item.name} className="flex flex-row items-start space-x-3 space-y-0">
                             <FormControl>
                               <Checkbox
-                                checked={field.value?.filter(item =>
-                                  data?.technology.some(tech => tech.name === item.name)
-                                )}
+                                checked={field.value?.includes(item.name)}
                                 // onCheckedChange={(checked) => {
                                 //   return checked
                                 //     ? field.onChange([...field.value, item.id])
@@ -154,11 +151,9 @@ const AddEditProjectForm: React.FC<TAddEditProjectForm> = ({ data, technology })
                                 // }}
                               />
                             </FormControl>
-                            <FormLabel className="text-sm font-normal">
-                              {item.name}
-                            </FormLabel>
+                            <FormLabel className="text-sm font-normal">{item.name}</FormLabel>
                           </FormItem>
-                        )
+                        );
                       }}
                     />
                   ))}
